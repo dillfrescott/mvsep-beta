7d6ac7581937d6280cda112e590d13d0f56fde0b
Cross <cross@dill.moe>
2025-01-16 11:36:47 -0500

    Save last 3 checkpoints

--- mvsep.py
+++ mvsep.py
@@ -280,6 +280,9 @@ def train(model, dataloader, optimizer, scheduler, loss_fn, device, epochs, chec
     avg_loss = 0.0
     loss_log = []
 
+    # List to keep track of checkpoint filenames
+    checkpoint_files = []
+
     if checkpoint_path:
         checkpoint = torch.load(checkpoint_path, map_location=device, weights_only=False)
         model.load_state_dict(checkpoint['model_state_dict'], strict=False)
@@ -343,6 +346,15 @@ def train(model, dataloader, optimizer, scheduler, loss_fn, device, epochs, chec
                     'avg_loss': avg_loss,
                     'loss_log': loss_log
                 }, checkpoint_filename)
+
+                # Add the new checkpoint to the list
+                checkpoint_files.append(checkpoint_filename)
+
+                # Remove the oldest checkpoint if more than 3 checkpoints exist
+                if len(checkpoint_files) > 3:
+                    oldest_checkpoint = checkpoint_files.pop(0)
+                    if os.path.exists(oldest_checkpoint):
+                        os.remove(oldest_checkpoint)
 
     # Save final loss log
     torch.save({'loss_log': loss_log}, 'loss_log.pt')
