2366ad7b84eb0ffcd691d7d8e0d416b55860f3b4
Cross <cross@dill.moe>
2025-04-22 19:05:07 -0400

    Revert

--- mvsep.py
+++ mvsep.py
@@ -307,10 +307,8 @@ def loss_fn(pred_vocal_mask,
         fft_sizes=[1024, 2048, 8192],
         hop_sizes=[256, 512, 2048],
         win_lengths=[1024, 2048, 8192],
-        perceptual_weighting=True,
+        perceptual_weighting=False,
         sample_rate=44100,
-        scale="mel",
-        n_bins=128,
         device=device
     )
 
