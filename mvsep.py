bf5855ca3531cee7af7e353b16884b530078e4d4
Cross <cross@dill.moe>
2025-05-29 22:00:17 -0400

    Talking heads attention

--- mvsep.py
+++ mvsep.py
@@ -32,7 +32,8 @@ class NeuralModel(nn.Module):
             heads=heads,
             ff_glu=True,
             rotary_pos_emb=True,
-            attn_flash=True
+            attn_pre_talking_heads=True,
+            attn_post_talking_heads=True
         )
         self.decoder = Decoder(
             dim=embed_dim,
@@ -40,7 +41,8 @@ class NeuralModel(nn.Module):
             heads=heads,
             ff_glu=True,
             rotary_pos_emb=True,
-            attn_flash=True
+            attn_pre_talking_heads=True,
+            attn_post_talking_heads=True
         )
         self.output_proj = nn.Linear(embed_dim, freq_bins * self.out_masks)
 
