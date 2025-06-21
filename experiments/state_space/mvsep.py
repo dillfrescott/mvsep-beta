d38d6ba24a5f77cac76ae51715716e9112a2d9f4
Cross Nastasi <cross@dill.moe>
2025-06-21 00:46:36 -0400

    Add reqs for state space and remove import

--- experiments/state_space/mvsep.py
+++ experiments/state_space/mvsep.py
@@ -4,7 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchaudio
-import librosa
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 from prodigyopt import Prodigy
