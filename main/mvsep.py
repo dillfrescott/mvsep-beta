6de2e5cdf885829867ed4b4c29e5835c51938cbe
Cross Nastasi <cross@dill.moe>
2025-12-28 10:42:24 -0500

    pope

--- main/mvsep.py
+++ main/mvsep.py
@@ -33,7 +33,7 @@ class NeuralModel(nn.Module):
             dim=embed_dim,
             depth=depth,
             heads=heads,
-            rotary_pos_emb=True
+            polar_pos_emb=True
         )
         self.output_proj = nn.Linear(embed_dim, freq_bins * self.out_masks * 2)
 
